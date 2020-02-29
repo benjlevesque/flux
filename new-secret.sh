5970afe0db51e29cf394f2bf165e1cacc8a155aa
Benjamin LEVESQUE <14175665+benjlevesque@users.noreply.github.com>
2020-02-29 01:35:48 +0100

    test secrets

---
+++ new-secret.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+echo "Enter the secret name";
+read NAME
+
+echo "Enter the secret value";
+read -s VALUE
+
+mkdir -p secrets
+
+
+SECRET=$(kubectl create secret generic $NAME --dry-run --from-literal=$NAME="$VALUE" -o json)
+echo $SECRET | kubeseal --controller-name sealed-secrets --format yaml  > secrets/$NAME.yaml
+
+yq d -i secrets/$NAME.yaml metadata.creationTimestamp 
+yq d -i secrets/$NAME.yaml spec.template.metadata.creationTimestamp 
+yq d -i secrets/$NAME.yaml status 
+
+#rm secret.yaml
